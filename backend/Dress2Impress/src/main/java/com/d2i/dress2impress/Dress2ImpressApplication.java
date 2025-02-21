2f35e8433b9ba4dbf121dd79084742e347648116
p.legide <p.legide@udc.es>
2025-02-22 00:02:10 +0100

    Initial backend commit

---
+++ backend/Dress2Impress/src/main/java/com/d2i/dress2impress/Dress2ImpressApplication.java
@@ -0,0 +1,13 @@
+package com.d2i.dress2impress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dress2ImpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dress2ImpressApplication.class, args);
+	}
+
+}
---
+++ backend/Dress2Impress/src/test/java/com/d2i/dress2impress/Dress2ImpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d2i.dress2impress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dress2ImpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
