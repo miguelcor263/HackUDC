c67fc9013ee142fd0f24e6bd2ada25a98c417d0f
p.legide <p.legide@udc.es>
2025-02-22 11:39:19 +0100

    Added Dtos for entities

---
+++ backend/Dress2Impress/src/main/java/com/d2i/dto/PriceDto.java
@@ -0,0 +1,13 @@
+package com.d2i.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceDto {
+    private String currency;
+    private PriceValueDto value;
+}
